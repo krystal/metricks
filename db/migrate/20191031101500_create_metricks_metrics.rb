37e26c5e7f8a576d13d956cbdae426100555cc26
Adam Cooke <me@adamcooke.io>
2019-10-31 16:40:13 +0000

    Rename migration and add additional indexes

--- db/migrate/20191031101500_create_metricks_metrics.rb
+++ db/migrate/20191031101500_create_metricks_metrics.rb
@@ -1,4 +1,4 @@
-class CreateMetrics < ActiveRecord::Migration[5.2]
+class CreateMetricksMetrics < ActiveRecord::Migration[5.2]
   def change
     create_table 'metrics' do |t|
       t.integer :type
@@ -10,7 +10,11 @@ class CreateMetrics < ActiveRecord::Migration[5.2]
       t.index %i[type association_1 time], name: 'on_assoc_1'
       t.index %i[type association_2 time], name: 'on_assoc_2'
       t.index %i[type association_3 time], name: 'on_assoc_3'
+      t.index %i[type association_4 time], name: 'on_assoc_4'
+      t.index %i[type association_5 time], name: 'on_assoc_5'
       t.index %i[type association_1 association_2 time], name: 'on_assoc_1_and_2'
+      t.index %i[type association_1 association_2 association_3 time], name: 'on_assoc_1_and_2_and_3'
+      t.index %i[type association_1 association_2 association_3 association_4 association_5 time], name: 'on_all_assocs'
     end
   end
 end
